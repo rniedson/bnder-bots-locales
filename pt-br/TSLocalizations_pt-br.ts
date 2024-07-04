ca33df106acda6ca704c3db86912c0ff8cef5db5
Robson Niedson <rniedson@gmail.com>
2024-07-04 10:54:10 -0300

    Verbesserung der Benachrichtigungsnachrichten auf Portugiesisch (pt-BR)
    - Aktualisierung der Aufgabenbenachrichtigungen für einen ansprechenderen und klareren Ton
    - Verbesserungen bei Frist-, Zuweisungs-, Kommentar- und Wiederholungsbenachrichtigungen für Aufgaben
    - Anpassungen der Webhook-Lognachrichten für mehr Klarheit und Kohärenz
    - Überarbeitung der Ereignisnachrichten für mehr Anziehungskraft und Verständnis
    - Aufrechterhaltung der Konsistenz bei der Verwendung von Begriffen und Formatierung

--- pt-br/TSLocalizations_pt-br.ts
+++ pt-br/TSLocalizations_pt-br.ts
@@ -4,36 +4,36 @@ export class TSLocalizations_pt_br {
     static Localizations: TSLocalizationKeys = {
         task: {
             deadline_due(id: string, deadline: string, guildName: string, guildId: string): string {
-                return `O prazo para a tarefa **${id}** vence em ${deadline} no servidor [${guildName}](https://discord.com/channels/${guildId})`;
+                return `O prazo da tarefa **${id}** é ${deadline} no servidor [${guildName}](https://discord.com/channels/${guildId})`;
             },
             assigned_notification(guildName: string, guildId: string): string {
-                return `Você foi atribuído a uma tarefa no servidor [${guildName}](https://discord.com/channels/${guildId})`;
+                return `Você foi designado para uma nova tarefa no servidor [${guildName}](https://discord.com/channels/${guildId})`;
             },
             assigned_embed_title(id: string): string {
                 return `Tarefa ${id} atribuída`;
             },
             assigned_embed_footer: "Essas notificações podem ser desativadas por um administrador do servidor usando o comando \"/preferences direct_messages\"",
-            assigned_embed_button_in_progress: "Marcar como em progresso",
-            assigned_embed_button_done: "Marcar como feito",
+            assigned_embed_button_in_progress: "Marcar como em andamento",
+            assigned_embed_button_done: "Marcar como concluída",
             deadline_was_in_past(id: string, guildName: string, guildId: string): string {
-                return `O prazo para a tarefa ${id} está atrasado no servidor [${guildName}](https://discord.com/channels/${guildId})`;
+                return `A tarefa ${id} está com o prazo vencido no servidor [${guildName}](https://discord.com/channels/${guildId})`;
             },
             repeat_notification(guildName: string, guildId: string): string {
-                return `Uma tarefa à qual você está atribuído foi repetida e precisa ser feita no servidor [${guildName}](https://discord.com/channels/${guildId})`;
+                return `Uma tarefa à qual você foi designado foi repetida e precisa ser concluída no servidor [${guildName}](https://discord.com/channels/${guildId})`;
             },
             repeat_notification_embed_title(id: string): string {
                 return `Tarefa ${id} repetida`;
             },
-            deadline_alert: "Prazo da tarefa",
+            deadline_alert: "Alerta de prazo",
             commented_embed_title(id: string): string {
-                return `Comentário adicionado à tarefa ${id}`;
+                return `Novo comentário na tarefa ${id}`;
             },
             commented_notification(guildName: string, guildId: string, author: string, commentText: string): string {
-                return `Um comentário foi adicionado a uma tarefa no servidor [${guildName}](https://discord.com/channels/${guildId}) por ${author}: ${commentText}`;
+                return `Um comentário foi adicionado à uma tarefa no servidor [${guildName}](https://discord.com/channels/${guildId}) por ${author}: ${commentText}`;
             }
         },
         webhookLogging: {
-            id_field: "Id",
+            id_field: "ID",
             deadline_field: "Prazo",
             assignees_field: "Atribuídos",
             status_field: "Estado",
@@ -46,50 +46,50 @@ export class TSLocalizations_pt_br {
             user_field: "Usuário",
             preset_field: "Predefinição",
             priority_field: "Prioridade",
-            label_field: "Rótulo",
+            label_field: "Etiqueta",
             personal_field: "Pessoal",
             manager_field: "Gerente",
-            repeat_interval_field: "Intervalo de repetição em dias",
+            repeat_interval_field: "Intervalo de repetição (dias)",
             repeat_end_date_field: "Data de término da repetição",
             status_field_value(status: number): string {
                 switch (status) {
                     case 0:
-                        return "Todo";
+                        return "A fazer";
                     case 1:
-                        return "Em progresso";
+                        return "Em andamento";
                     case 2:
-                        return "Feito";
+                        return "Concluído";
                     default:
                         return "Desconhecido";
                 }
             },
             deadline_due: {
                 title(id: string): string {
-                    return `Prazo para a tarefa ${id}`;
+                    return `Prazo da tarefa ${id}`;
                 },
                 description(id: string, deadline: string): string {
-                    return `O prazo para a tarefa **${id}** vence em ${deadline}`;
+                    return `O prazo da tarefa **${id}** é ${deadline}`;
                 }
             },
             task_deleted: {
                 title(id: string): string {
-                    return `Tarefa ${id} excluída.`;
+                    return `Tarefa ${id} excluída`;
                 },
                 description(id: string): string {
-                    return `A tarefa ${id} foi excluída da lixeira.`;
+                    return `A tarefa ${id} foi removida da lixeira.`;
                 }
             },
             repeat_set: {
                 title(id: string): string {
-                    return `Tarefa ${id} repetida.`;
+                    return `Tarefa ${id} repetida`;
                 },
                 description(id: string): string {
-                    return `A tarefa ${id} foi repetida e o estado foi definido como todo.`;
+                    return `A tarefa ${id} foi repetida e o status foi redefinido.`;
                 }
             },
             task_created: {
                 title(id: string): string {
-                    return `Tarefa ${id} criada.`;
+                    return `Tarefa ${id} criada`;
                 },
                 description(task_text: string, id: string): string {
                     return `A tarefa ${id} foi criada com o texto: ${task_text}`;
@@ -97,22 +97,22 @@ export class TSLocalizations_pt_br {
             },
             task_updated: {
                 title(id: string): string {
-                    return `Tarefa ${id} atualizada.`;
+                    return `Tarefa ${id} atualizada`;
                 },
                 description(id: string): string {
-                    return `A tarefa ${id} foi atualizada e os seguintes dados foram alterados.`;
+                    return `A tarefa ${id} foi atualizada e as seguintes informações foram alteradas.`;
                 }
             },
             project_created: {
                 title: "Projeto criado",
                 description(projectName: string): string {
-                    return `Projeto ${projectName} foi criado`;
+                    return `O projeto ${projectName} foi criado.`;
                 }
             },
             project_deleted: {
                 title: "Projeto excluído",
                 description(projectName: string): string {
-                    return `Projeto ${projectName} foi excluído`;
+                    return `O projeto ${projectName} foi excluído.`;
                 }
             },
             task_status_changed: {
@@ -127,22 +127,22 @@ export class TSLocalizations_pt_br {
                 title: "Permissão revogada",
             },
             preset_applied: {
-                title: "Preset aplicado",
+                title: "Predefinição aplicada",
             },
             deadline_was_in_past: {
                 title(id: string): string {
-                    return `O prazo para a tarefa ${id} está atrasado`;
+                    return `Prazo da tarefa ${id} vencido`;
                 }
             },
             project_updated: {
                 title: "Projeto atualizado",
                 description(name: string): string {
-                    return `Projeto ${name} foi atualizado`;
+                    return `O projeto ${name} foi atualizado.`;
                 }
             },
             test_message: {
-                title: "Esta é uma mensagem de teste",
-                description: "Se você vê esta mensagem, o webhook está funcionando corretamente e o log está ativado."
+                title: "Mensagem de teste",
+                description: "Se você está vendo esta mensagem, o webhook está funcionando corretamente e o log está ativo."
             },
             comment_added: {
                 title(id: string): string {
@@ -157,7 +157,7 @@ export class TSLocalizations_pt_br {
         },
         event: {
             reminderDirectMessage(id: string, name: string, durationTillEventMinutes: number, guildName: string, guildId: string, startTime: string): string {
-                return `O próximo evento (${id}: ${name}) começará em ${durationTillEventMinutes} minutos em [${guildName}](https://discord.com/channels/${guildId}).`;
+                return `O próximo evento (${id}: ${name}) começará em ${durationTillEventMinutes} minutos no servidor [${guildName}](https://discord.com/channels/${guildId}).`;
             },
             invite_notification: {
                 content(inviterId: string, eventId: string, eventName: string, start: string, end: string): string {
